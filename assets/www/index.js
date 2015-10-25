3df157430779c03d8efc2ec32874a898d4371f12
Nick <Nikhilpancholi6@gmail.com>
2015-10-25 13:44:53 +0530

    myapp

---
+++ assets/www/index.js
@@ -0,0 +1,10 @@
+
+ 
+ function preloader(){
+            document.getElementById("loading").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }//preloader
+        window.onload = preloader;
+		
+		
+ 
